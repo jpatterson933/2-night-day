938ff5b73336682ae5c0f1a86ba09396cba6cc74
Jeff <jpatterson933@ucla.edu>
2022-01-26 07:29:12 -0800

    Basic grid structure setup for one page website

--- client/src/views/landing.js
+++ client/src/views/landing.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
+// Stylesheet
+import './index.css';
+
 export const Landing = () => {
     return (
         <div id="main-grid">
-            <div>Row 1</div>
-            <div>Row 2</div>
-            <div>Row 3</div>
-            <div>Row 4</div>
-            <div>Row 5</div>
-            <div>Row 6</div>
-            <div>Row 7</div>
-            <div>Row 8</div>
+            <div id="company">Night and Day Profile</div>
+            <div id="owner">About Julien Calella</div>
+            <div id="project-one">Project 1</div>
+            <div id="main-packages">Main Consulting Packages</div>
+            <div id="project-two">Project 2</div>
+            <div id="secondary-packages">Secondary Consulting Packages</div>
+            <div id="other-projects">Other Projects</div>
+            <div id="contact">Contact</div>
         </div>
     )
 }
