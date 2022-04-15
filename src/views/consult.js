43dca4fffd0e2d3e41ffcb1cd5707e887954c008
Jeffery Patterson <jpatterson933@ucla.edu>
2022-04-15 15:47:09 -0700

    added the consult cards from the previous night and day website. Removed the componenet box from the majority of pages. Also added the consulting cards to the program cards. Removed the color of the nav bar and buttons so as to much the page keeping them see through.

--- src/views/consult.js
+++ src/views/consult.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 // components
-import { Box } from '../components/Box';
+import { ConsultingCard } from '../components/Consult';
+
 
 // Stylesheet
 import './index.css';
@@ -9,18 +10,8 @@ import './button.css';
 
 export const Consult = () => {
     return (
-            <div id="main-grid">
-                <h1>Consulting</h1>
-                <div id="main-packages">
-                    <Box className="package-one" consultTitle="Bar Program" detailsOne="Bar Layout" detailsTwo="Cocktail Creation" detailsThree="Staff Training" />
-                    <Box className="package-two" consultTitle="Bar Design" detailsOne="Maximize Space" detailsTwo="Increase Volume" detailsThree="Professional Layout" />
-                    <Box className="package-three" consultTitle="Bar Sales" detailsOne="Increased Sales" detailsTwo="Inventory Turnover" detailsThree="Great Taste" />
-                </div>
-                <div id="secondary-packages">
-                    <Box className="package-one" consultTitle="Menu Design" detailsOne="Improved Sales" detailsTwo="Streamline Orders" detailsThree="Easier Sales for Staff" />
-                    <Box className="package-two" consultTitle="Lower Costs" detailsOne="Lower Costs through" detailsTwo="Inventory Tracking" detailsThree="Staff Training" />
-                    <Box className="package-three" consultTitle="Prep Program" detailsOne="Prep Design" detailsTwo="Fresh Daily" detailsThree="Minimize Waste" />
-                </div>
-            </div>
+        <>
+            <ConsultingCard />
+        </>
     )
 }
