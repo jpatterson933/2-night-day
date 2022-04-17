45d4fb578f5169803ff6933cbfc508b6574660ae
Jeffery Patterson <jpatterson933@ucla.edu>
2022-04-16 20:55:45 -0700

    fixed everything back to original state ALWAYS USE A DIFFERENT BRANCH WHEN MESSING ABOUT

--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 window.env = {
-    "SERVICE_KEY": "service_goh9scj",
-    "TEMPLATE_KEY": "template_l08gz6a",
-    "USER_KEY": "user_yeg4gXRakgFfPbOvUdrJw"
+  "SERVICE_KEY": "service_goh9scj",
+  "TEMPLATE_KEY": "template_l08gz6a",
+  "USER_KEY": "user_yeg4gXRakgFfPbOvUdrJw"
 };
